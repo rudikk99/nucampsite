939b8e3bfaeabe980b670b8ff102d56c0345a3ec
rudikk99 <rudikk99@yahoo.com>
2020-11-06 15:14:26 -0500

    w5 Fetch from Server

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -8,6 +8,7 @@ import { Breadcrumb, BreadcrumbItem,
 import { Link } from 'react-router-dom';  
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 
 const required = val => val && val.length;
@@ -117,10 +118,10 @@ function RenderCampsite({campsite}) {
     return (
         <div className="col-md-5 m-1"> 
             <Card>
-                <CardImg top src={campsite.image} alt={campsite.name} />
-                    <CardBody>
-                        <CardText>{campsite.description}</CardText>
-                    </CardBody>
+                <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
+                <CardBody>
+                    <CardText>{campsite.description}</CardText>
+                </CardBody>
             </Card>
         </div> 
     );
--- src/redux/partners.js
+++ src/redux/partners.js
@@ -1,3 +1,23 @@
+//import * as ActionTypes from './ActionTypes';
+
+//export const Partners = (state = { isLoading: true,
+//                                  errMess: null,
+//                                    partners: [] }, action) => {
+//    switch (action.type) {
+//        case ActionTypes.ADD_PARTNERS:
+//            return {...state, isLoading: false, errMess: null, partners: action.payload};
+
+//        case ActionTypes.PARTNERS_LOADING:
+//            return {...state, isLoading: true, errMess: null, partners: []}
+
+//        case ActionTypes.PARTNERS_FAILED:
+//            return {...state, isLoading: false, errMess: action.payload};
+            
+//        default:
+//            return state;
+//      }
+//};
+
 import { PARTNERS } from '../shared/partners';
 
 export const Partners = (state = PARTNERS, action) => {
@@ -5,4 +25,4 @@ export const Partners = (state = PARTNERS, action) => {
         default:
             return state; 
     }
-}
+};
